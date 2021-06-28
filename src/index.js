ff79f29f933ad96fecce9a54c1e1bca8865f84d3
Vladyslav <vlad.motuz@gmail.com>
2021-06-28 23:42:52 +0300

    add js

--- src/index.js
+++ src/index.js
@@ -10,5 +10,40 @@ const createCardItem = (cards) => {
 }
 menu.insertAdjacentHTML('beforeend', createCardItem(menuCards));
 
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const body = document.querySelector('body');
 const input = document.querySelector('#theme-switch-toggle');
-console.log(input);
+
+const setClasslist = (addClassList, removeClassList) => {
+    body.classList.add(`${addClassList}`);
+    body.classList.remove(`${removeClassList}`);
+};
+
+const onInputTheme = (e) => {
+    if (input.checked) {
+        setClasslist(Theme.DARK, Theme.LIGHT);
+    } else {
+        setClasslist(Theme.LIGHT, Theme.DARK);
+    }   
+};
+
+const settingThemeLocalhost = (e) => {
+    if (input.checked) {
+        localStorage.setItem('theme', Theme.DARK);
+    } else {
+        localStorage.removeItem('theme');
+        localStorage.setItem('theme', Theme.LIGHT);
+    }
+};
+
+if (localStorage.getItem('theme') === Theme.DARK) {
+    body.classList.add(Theme.DARK);
+    input.checked = true;
+};
+
+input.addEventListener('change', onInputTheme);
+input.addEventListener('change', settingThemeLocalhost);
